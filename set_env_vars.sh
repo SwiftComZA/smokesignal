105105a838d793e0e10814c7abf4485f3452379e
Laderlappen <elmervermeulen@gmail.com>
2023-08-29 10:43:49 +0200

    Added env var file

---
+++ set_env_vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export ENV=development
+export ETH_PROVIDER_URL=https://mainnet.infura.io/v3/715f42b35f9a4792a8050f284e61bab5
+export XDAI_PROVIDER_URL=https://rpc.gnosischain.com/
+export ZKTEST_PROVIDER_URL=https://testnet.era.zksync.dev
+export SCROLLTESTNET_PROVIDER_URL=https://sepolia-rpc.scroll.io
+export BASETESTNET_PROVIDER_URL=https://base-goerli.g.alchemy.com/v2/TGyo6QAW98PF3MOzvaRk8OwPkv747rFA
+export SHARDEUMTESTNET_PROVIDER_URL=https://sphinx.shardeum.org/
+export FAUCET_TOKEN=0000000000000000
+export FB_TRACKING_ID=000000000
+export GA_TRACKING_ID=0000000000000000
